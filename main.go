rakyll/gotest

commit 29d015ff1f015bc159b4668edec477e7e8ca9a08

Renamed variables to reflect test results (#23)

main.go
@@ -21,9 +21,9 @@ import (
 )
 
 var (
-	success = color.FgGreen
-	skipped = color.FgYellow
-	fail = color.FgHiRed
+	pass = color.New(color.FgGreen)
+	skip = color.New(color.FgYellow)
+	fail = color.New(color.FgHiRed)
 )
 
 const paletteEnv = "GOTEST_PALETTE"
@@ -80,23 +80,28 @@ func parse(line string) {
 	defer color.Unset()
 
 	switch {
-	// success
+	case strings.HasPrefix(trimmed, "=== RUN"):
+		fallthrough
+	case strings.HasPrefix(trimmed, "?"):
+		c = nil
+
+	// passed
 	case strings.HasPrefix(trimmed, "--- PASS"):
 		fallthrough
 	case strings.HasPrefix(trimmed, "ok"):
 		fallthrough
 	case strings.HasPrefix(trimmed, "PASS"):
-		color.Set(success)
+		c = pass
 
 	// skipped
 	case strings.HasPrefix(trimmed, "--- SKIP"):
-		color.Set(skipped)
+		c = skip
 
-	// failure
+	// failed
 	case strings.HasPrefix(trimmed, "--- FAIL"):
 		fallthrough
 	case strings.HasPrefix(trimmed, "FAIL"):
-		color.Set(fail)
+		c = fail
 	}
 
 	fmt.Printf("%s\n", line)
@@ -131,7 +136,7 @@ func setPalette() {
 		fail = c
 	}
 	if c, ok := colors[vals[1]]; ok {
-		success = c
+		pass = color.New(c)
 	}
 }
 

commit e60feffe81351ec137a51b926ec6867ed20fcbe2

Fix the colors broken by #23

main.go
@@ -22,9 +22,9 @@ import (
 )
 
 var (
-	pass = color.New(color.FgGreen)
-	skip = color.New(color.FgYellow)
-	fail = color.New(color.FgHiRed)
+	pass = color.FgGreen
+	skip = color.FgYellow
+	fail = color.FgHiRed
 )
 
 const paletteEnv = "GOTEST_PALETTE"
@@ -94,17 +94,16 @@ func consume(wg *sync.WaitGroup, r io.Reader) {
 	}
 }
 
-var c *color.Color
-
 func parse(line string) {
 	trimmed := strings.TrimSpace(line)
 	defer color.Unset()
 
+	var c color.Attribute
 	switch {
 	case strings.HasPrefix(trimmed, "=== RUN"):
 		fallthrough
 	case strings.HasPrefix(trimmed, "?"):
-		c = nil
+		color.Unset()
 
 	// passed
 	case strings.HasPrefix(trimmed, "--- PASS"):
@@ -125,6 +124,7 @@ func parse(line string) {
 		c = fail
 	}
 
+	color.Set(c)
 	fmt.Printf("%s\n", line)
 }
 
@@ -154,10 +154,10 @@ func setPalette() {
 		return
 	}
 	if c, ok := colors[vals[0]]; ok {
-		fail = color.Set(c)
+		fail = c
 	}
 	if c, ok := colors[vals[1]]; ok {
-		pass = color.New(c)
+		pass = c
 	}
 }
 